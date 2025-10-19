36d2845357a29337a271caefafd8faf9f86be9ca
18gen <ichihashigen@gmail.com>
2025-10-19 22:23:15 +0900

    Initial Commit

---
+++ AREigoTests/AREigoTests.swift
@@ -0,0 +1,17 @@
+//
+//  AREigoTests.swift
+//  AREigoTests
+//
+//  Created by Gen Ichihashi on 2025-10-19.
+//
+
+import Testing
+@testable import AREigo
+
+struct AREigoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
